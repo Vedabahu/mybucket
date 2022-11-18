2e843e9da870b186a8bca29d57774d05800ad608
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2022-11-18 22:29:07 +0530

    Updated convertors and some buckets

--- bin/reverse-convert.ps1
+++ bin/reverse-convert.ps1
@@ -1,6 +1,7 @@
-Get-ChildItem ".\bucket" -Filter *.json |
+Get-ChildItem "$PSScriptRoot\..\bucket" -Filter *.json |
 ForEach-Object {
     Get-Content $_.FullName | Out-String | ConvertFrom-Json | ConvertTo-Yaml | Out-File ".\bucket-yaml\$($_.BaseName).yaml"
     Write-Output "Comlpeted converting $($_.BaseName).json"
 }
+Write-Output "Testing"
 Write-Output "`n`nFully Completed."
